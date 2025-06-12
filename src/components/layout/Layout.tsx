2d3edb681c84266364bd9fa9a6c9e7e7e5a20f58
rkterungwa16 <terungwakombol@gmail.com>
2025-06-12 03:11:06 +0100

    include mdn link for box sizing css property

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -56,6 +56,7 @@ export const Layout: FC<ILayout> = ({ children }) => {
                  * the padding added to that element will be added to the total width of the element.
                  * You will see the effect of this as the sidebar extends into the main content area.
                  * This behaviour occurs for the css property box-sizing: content-box;
+                 * https://developer.mozilla.org/en-US/docs/Web/CSS/box-sizing
                  */
                 padding: "0.5rem",
               }}
